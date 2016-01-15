2e8633eb3541ad84aab781333f9cee7a490fbb06
Yeamy <Yeamy0754@Yeamys-MacBook-Pro.local>
2016-01-15 22:36:59 +0800

    bug fix and divider color modify

--- app/src/main/java/com/yeamy/daily/app/ResourcesCompact.java
+++ app/src/main/java/com/yeamy/daily/app/ResourcesCompact.java
@@ -3,13 +3,12 @@ package com.yeamy.daily.app;
 import android.content.Context;
 import android.os.Build;
 
+@Deprecated
 public class ResourcesCompact {
 
     public static int getColor(Context context, int resId) {
         if (Build.VERSION.SDK_INT >= 23){
             return context.getColor(resId);
-        } else if (Build.VERSION.SDK_INT >= 17) {
-            return context.getResources().getColor(resId, context.getTheme());
         } else {
             return context.getResources().getColor(resId);
         }
